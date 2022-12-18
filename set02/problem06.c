6bfed82c68d7098124ce5781f3a493a5d131f4c8
alaricseq <115693693+Alaricseq@users.noreply.github.com>
2022-12-18 16:25:01 +0000

    update

---
+++ set02/problem06.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+
+int main()
+{
+  char a[100];
+  char reverse_a[100];
+  
+  
+}
+void input_string(char *a)
+{
+  printf("Enter a string:\n");
+  scanf("%s",a);
+}
+
+void str_reverse(char *str, char *rev_str)
+{
+  
+}
+
+void output(char *a, char *reverse_a)
+{
+    
+}
