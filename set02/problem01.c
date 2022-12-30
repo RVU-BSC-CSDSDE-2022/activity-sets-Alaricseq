e139e3002d72a3611ecf07edab06a27bbaca410f
alaricseq <115693693+Alaricseq@users.noreply.github.com>
2022-12-30 18:48:22 +0000

    updated

--- set02/problem01.c
+++ set02/problem01.c
@@ -5,24 +5,23 @@ void output(float base, float height, float area);
 
 int main()
 {
-  float base, height, area;
+  float base,height,area;
   input(base,height);
 }
 
-void input(float base,float height)
+void input(float base, float height)
 {
   float area;
-  printf("Enter base and height");
+  printf("Enter base and height of a triangle:");
   if(scanf("%f%f",&base,&height));
   find_area(base,height,&area);
 }
-
 void find_area(float base , float height, float *area)
 {
-  *area= (base*height)*1/ 2 ;
-  output(base,height, *area);
+  *area=(base*height)*1/2;
+  output(base,height,*area);
 }
 void output(float base, float height, float area)
 {
-  printf("The area of the triangle with base %f and height %f is %f",base,height,area);
+  printf("The area of the traingle with base %f and height %f is %f",base,height,area);
 }
