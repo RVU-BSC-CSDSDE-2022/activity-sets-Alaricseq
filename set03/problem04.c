cc506417b4e486882974d1c2c5064fc9256d3d8e
alaricseq <115693693+Alaricseq@users.noreply.github.com>
2023-01-06 14:05:28 +0000

    updated

---
+++ set03/problem04.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int input();
+int find_fibo(int n);
+void output(int n, int fibo);
+
+int main()
+{
+  
+}
+int input()
+{
+  int n;
+  printf("Enter the number:\n");
+  if(scanf("%d",&n));
+  return n;
+}
+int find_fibo(int n)
+{
+  
+}
+void output(int n, int fibo)
+{
+  printf("fibo(%d) = %d",n,fibo);
+}
