ac386576ed8c8f4665c560028b99e7b86284e500
alaricseq <115693693+Alaricseq@users.noreply.github.com>
2023-01-08 12:35:14 +0000

    updated

--- set01/problem11.c
+++ set01/problem11.c
@@ -15,11 +15,10 @@ int main()
   Complex a,b,sum;
   printf("Enter a and b where a + bi is the First complex number.\n");
   a=input_complex();
-  printf("Enter a and b where c + di is the Second complex number.\n");
+  printf("Enter a and b where a + bi is the First complex number.\n");
   b=input_complex();
   sum=add_complex(a,b);
   output(a,b,sum);
-  return 0;
 }
 
 Complex input_complex()
@@ -28,15 +27,17 @@ Complex input_complex()
   if(scanf("%f%f",&c1.real,&c1.imaginary));
   return c1;
 }
+
 Complex add_complex(Complex a, Complex b)
 {
   Complex sum;
-  sum.real=a.real + b.real;
-  sum.imaginary=a.imaginary + b.imaginary;
+  sum.real= a.real + b.real;
+  sum.imaginary= a.imaginary + b.imaginary;
   return sum;
 }
+
 void output(Complex a, Complex b, Complex sum)
 {
-  printf("The sum of 2 complex numbers are %d + %di\n",(int)sum.real, (int)sum.imaginary);
+  printf("The sum of %d + %di and %d + %di is %d + %di\n",(int)a.real,(int)a.imaginary,(int)b.real,(int)b.imaginary,(int)sum.real,(int)sum.imaginary);
   return 0;
 }
--- set03/problem04.c
+++ set03/problem04.c
@@ -8,19 +8,33 @@ int main()
   int n,fibo;
   n=input();
   fibo=find_fibo(n);
-  ouput(n,fibo);
-  return0;  
+  output(n,fibo);
+  return 0;  
 }
 int input()
 {
   int n;
-  printf("Enter the number:\n");
+  printf("Enter the number: ");
   if(scanf("%d",&n));
   return n;
 }
 int find_fibo(int n)
 {
-  
+  int fibo;
+  if (n==1)
+  {
+    return 0;
+  }
+  else if (n==2)
+  {
+    return 1;
+  }
+  else if (n==3)
+  {
+    return 1;
+  }
+  else 
+    return find_fibo(n-1) + find_fibo(n-2);
 }
 void output(int n, int fibo)
 {
