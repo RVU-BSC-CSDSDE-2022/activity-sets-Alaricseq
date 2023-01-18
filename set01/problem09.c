9188a304b4e79bf23021b329fc95a01d13a6e6b0
alaricseq <115693693+Alaricseq@users.noreply.github.com>
2023-01-18 18:01:24 +0000

    updated

--- set01/problem09.c
+++ set01/problem09.c
@@ -3,26 +3,28 @@
 float input();
 float square_root(float n);
 void output(float n, float sqrroot);
+
 int main()
 {
-  double n ,sqrroot;
-  n=input();
-  sqrroot=square_root(n);
-  output(n, sqrroot);
-}  
+  
+}
+
 float input()
 {
   double n;
   printf("Enter the number: ");
-  if(scanf("%lf",&n));
+  if(scanf("%lf",&lf));
   return n;
 }
+
 float square_root(float n)
 {
-  double sqrroot=sqrt(n);
+  double sqrroot;
+  sqrroot = sqrt(n);
   return sqrroot;
 }
-void output(float n ,float sqrroot)
+
+void output(float n, float sqrroot)
 {
-  printf("Square root of %.2lf =  %.2lf", n , sqrroot);
+  printf("The square root of %.2lf = %.2lf\n",n,sqrroot);
 }
--- set01/problem10.c
+++ set01/problem10.c
@@ -4,29 +4,32 @@ int stringcompare(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
 int main()
 {
-  char string1[20], string2[20];
-  input_two_strings(&string1,&string2);
+  char string1[20] , string2[20];
+  input_two_strings(&string1, &string2);
 }
 
 void input_two_strings(char *string1, char *string2)
-{
-  printf("Enter the first string:\n");
-  if(scanf("%s",string1));
-  printf("Enter the second string:\n");
-  if(scanf("%s",string2));
-  stringcompare(string1,string2);
+{ 
+  printf("Enter the first string \n");
+  scanf("%s", string1);
+  printf("Enter the second string\n");
+  scanf("%s", string2);
+  stringcompare(string1,string2);  
 }
+
 int stringcompare(char *string1, char *string2)
 {
   int var=0,i=0;
-  while( string1[i] != '\0' && string2[i] != '\0')
+  while (string1[i] != '\0' && string2[i] != '\0')
     {
-      if( string1[i] > string2[i] )
+      
+      if(string1[i] > string2[i])
       {
         var=1;
         break;
       }
-      else if( string2[i] >string1[i] )
+      
+      else if (string2[i] > string1[i])
       {
         var=-1;
         break;
@@ -34,14 +37,15 @@ int stringcompare(char *string1, char *string2)
       i++;
     }
   
-  if(var==1)
-  {printf("First string %s is greater than the second string %s",string1,string2);}
+  if(var==1) 
+  {printf("First string %s is greater than the second string %s\n",string1,string2);}
   else if(var==-1)
-  {printf("Second string %s is greater than the first string %s",string2,string1);}
+  {printf("Second string %s is greater than the first string %s\n",string2,string1);}
   return 0;
   int result = stringcompare(string1,string2);
   output(string1,string2,result);
 }
+
 void output(char *string1, char *string2, int result)
 {
   printf("The output is %d",result);
