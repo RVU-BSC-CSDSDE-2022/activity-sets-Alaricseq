86d5744230b0968e525661aaab8a94d53583b20e
alaricseq <115693693+Alaricseq@users.noreply.github.com>
2023-01-22 17:15:00 +0000

    updated

--- set01/problem09.c
+++ set01/problem09.c
@@ -30,7 +30,7 @@
 // {
 //   printf("The square root of %.2lf = %.2lf\n",n,sqrroot);
 // }
-
+#include <math.h>
 #include <stdio.h>
 float input();
 float square_root(float n);
@@ -38,19 +38,15 @@ void output(float n, float sqrroot);
 
 int main()
 {
-  float n,sqrroot;
-  n=input();
-  sqrroot=square_root(n);
-  output(n,sqrroot);
-  return 0;
+  
 }
 
-float input()
+float intput()
 {
   float n;
   printf("Enter the number: ");
   if(scanf("%f",&n));
-  return n;
+  return n ;
 }
 
 float square_root(float n)
@@ -58,7 +54,7 @@ float square_root(float n)
   float guess,next;
   guess=n/2;
   next=0.5*(guess+n/guess);
-  while(fabs(next-guess)>0.001)/fabs(obsolute value)/
+  while(fabs(next-guess)>0.001)
     {
       guess=next;
       next=0.5*(guess+n/guess);
@@ -66,7 +62,7 @@ float square_root(float n)
   return next;
 }
 
-void output(float n; float sqrroot)
+void outpu(float n, float sqrroot)
 {
-  printf("square root of %f.2f is %.2f\n",n,sqrroot);
+  printf("Square root of %.2f is %.2f\n",n,sqrroot);
 }
